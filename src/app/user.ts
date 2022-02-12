6e0221a10a1b3acda3645465871a0e139830b51f
Teri11 <augustineteri157@gmail.com>
2022-02-12 17:27:12 +0300

    added constructor more selection

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,3 @@
 export class User {
-  constructor(public userName:string,public avatarUrl:string,public followers:number,public following:number,public repo:number){}
+  constructor(public avatarUrl:string,public name:string,public userName:string,public location:string,public followers:number,public following:number,public repo:number){}
 }
