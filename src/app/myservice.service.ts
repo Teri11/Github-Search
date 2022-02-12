786801467ba69363cf2476e7c436e3b09f4950d7
Teri11 <augustineteri157@gmail.com>
2022-02-12 17:28:12 +0300

    added data choices

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -11,14 +11,18 @@ export class MyserviceService {
   user!: User;
 
   constructor( ) { 
-   this.user=new User("","",0,0,0)
+   this.user=new User("","","","",0,0,0)
   }
 
   async myDetails(){
     try{const response = await axios.get("https://api.github.com/users/Teri11",{headers:{"Authorization":"environment.token"}})
-  //  console.log(response)
-    this.user.avatarUrl=response.data.avatar_url;
-   this.user.userName=response.data.login
+   console.log(response)
+   this.user.name=response.data.name;
+    this.user.avatarUrl=response.data.avatar_url,
+   this.user.userName=response.data.login,
+   this.user.location=response.data.location,
+   this.user.followers=response.data.followers,
+   this.user.following=response.data.following
   }catch(error){
     console.log(error)
   }
