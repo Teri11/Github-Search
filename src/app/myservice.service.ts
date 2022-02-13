a6ffc708f76a92aa37f2272eee9076ac5d2869b2
Teri11 <augustineteri157@gmail.com>
2022-02-13 07:18:45 +0300

    added repo call function

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -3,29 +3,48 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import axios from 'axios';
 import { environment } from 'src/environments/environment';
-
+import { Repo } from './repo';
 @Injectable({
   providedIn: 'root'
 })
 export class MyserviceService {
   user!: User;
+  repo!:Repo;
+  repos:Repo[]=[]
 
   constructor( ) { 
    this.user=new User("","","","",0,0,0)
+   this.repo=new Repo("","","","")
   }
 
   async myDetails(){
-    try{const response = await axios.get("https://api.github.com/users/Teri11",{headers:{"Authorization":"environment.token"}})
-   console.log(response)
-   this.user.name=response.data.name;
+    try{
+      
+   const response = await axios.get("https://api.github.com/users/Teri11",{headers:{"Authorization":environment.token}})
+   const myrepos= await axios.get("https://api.github.com/users/Teri11/repos")
+
+   const allRepos=myrepos.data;
+   for(let items of allRepos){
+     if(!items.description){
+       items.description="No Description"
+     }
+     this.repos.push(new Repo(items.name,items.description,items.html_url,items.language))
+   }
+
+
+    console.log(response)
+    console.log(this.repos)
+    this.user.name=response.data.name;
     this.user.avatarUrl=response.data.avatar_url,
-   this.user.userName=response.data.login,
-   this.user.location=response.data.location,
-   this.user.followers=response.data.followers,
-   this.user.following=response.data.following
+    this.user.userName=response.data.login,
+     this.user.location=response.data.location,
+     this.user.followers=response.data.followers,
+    this.user.following=response.data.following
+
+   
   }catch(error){
     console.log(error)
-  }
+   }
     
    
   }
