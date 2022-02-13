8b0661c16714d795d167a3c2580367734b825b83
Teri11 <augustineteri157@gmail.com>
2022-02-13 07:17:42 +0300

    imported and exported repo link

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { MyserviceService } from '../myservice.service';
-
+import { Repo } from '../repo';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -9,13 +9,17 @@ import { MyserviceService } from '../myservice.service';
 })
 export class DetailsComponent implements OnInit {
   me!:User;
-
+  myrepo!:Repo;
+  myrepos:Repo[]=[]
 
   constructor(private myService:MyserviceService) { }
 
   ngOnInit(): void {
     this.myService.myDetails();
     this.me=this.myService.user
+    this.myrepo=this.myService.repo
+    this.myrepos=this.myService.repos
+    
   }
 
 }
